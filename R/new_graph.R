aadbfeb389df4a0058a9e98d9510bb4caacfd5bc
Martin Frigaard <mjfrigaard@gmail.com>
2022-12-16 22:12:34 -0800

    new layout

---
+++ R/new_graph.R
@@ -0,0 +1,17 @@
+library(fs)
+library(janitor)
+
+new_graph <- function(name, section = c("uni", "amt", "prp", "dist", "rela", "stat")) {
+    sections <- c("uni", "amt", "prp", "dist", "rela", "stat")
+    # clean name
+    clean_name <- janitor::make_clean_names(string = name)
+    # create path
+    grph_pth <- paste0("./", section, "/", clean_name, ".qmd")
+    if (section %in% sections) {
+        fs::file_copy(path = "./_drafts/_layout.qmd", 
+            new_path = grph_pth)
+        fs::file_show(grph_pth)
+    } else {
+        cli::cli_abort("this is not a section")
+    }
+}
---
+++ build.R
@@ -0,0 +1,3 @@
+source("R/new_graph.R")
+
+new_graph(name = "more Heatmaps", section = "amt")
